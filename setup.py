b1ee95cd3885dee42afae3faf45ffb58cdbece43
injoon <injoon5@naver.com>
2021-03-01 18:48:37 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,44 @@
-from setuptools import setup
+import setuptools
 
-setup(
-    name='soojin_lib',
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='replalive',
     version='0.0.1',
-    description='soojin pip install test',
-    url='https://github.com/lsjsj92/git_pip_install.git',
-    author='leesoojin',
-    author_email='asfd',
-    license='soojin',
-    packages=['soojin_lib'],
-    zip_safe=False,
+    author='Injoon Oh',
+    author_email='injoon5@naver.com',
+    description='Use Repl.it for python project hosting!',
+    keywords='repl.it, repl, hosting, alive, always, always alive',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/injoon5/replalive',
+    project_urls={
+        'Source Code': 'https://github.com/injoon5/replalive'
+        # 'Funding': '',
+        # 'Say Thanks!': '',
+    },
+    package_dir={'': 'src'},
+    packages=['replalive'],
+    packages=setuptools.find_packages(where='src'),
+    classifiers=[
+        # see https://pypi.org/classifiers/
+        'Development Status :: 5 - Production/Stable',
+
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3 :: Only',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
     install_requires=[
-        'numpy==1.18.3'
+        'flask==1.1.2'
     ]
 )
