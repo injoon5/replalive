63f0a2b14b9d2042e53deee9905204285e3f6f40
injoon <injoon5@naver.com>
2021-03-01 18:55:19 +0900

    Update and rename soojin_lib/soojin.py to replalive/main.py

---
+++ replalive/main.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+@app.route('/')
+def main():
+    resres = "Your project is alive! now go <a href="https://repl.it/talk/learn/Hosting-discordpy-bots-with-replit/11008">here</a> and go to the 4th step, do the rest of it."
+    return resres
+    
+def run():
+    app.run(host="0.0.0.0", port=8080)
+    
+def keep_alive():
+    server = Thread(target=run)
+    server.start()
--- soojin_lib/soojin.py
+++
@@ -1,2 +0,0 @@
-def print_soojin(msg):
-    print(f"안녕하세요? 이수진입니다. {msg}")
